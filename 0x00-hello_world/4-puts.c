a089b286c8c30062698910e60e5bc9c9aae64e2c
KofiFloki <Jeremiah.martey@yahoo.com>
2022-12-08 11:42:13 -0800

    Task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a string via puts
+ * this is a program that prints out a string followed by a new line
+ * return int 0 when done with no errors
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
