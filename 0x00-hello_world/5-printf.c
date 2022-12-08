1e41d8c632403a2f393489f02e5762961963941e
KofiFloki <Jeremiah.martey@yahoo.com>
2022-12-08 12:03:01 -0800

    Task 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - prints out a string via printf
+ * Description: Program should print out the string followed by a line
+ * Return: int 0 when done with no errors
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
