9da2bc120ff48f3f690820a018545ca1a8a70781
KofiFloki <Jeremiah.martey@yahoo.com>
2023-04-07 17:00:03 -0700

    My project commit

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - a function that prints a hash table
+ * @ht: the hash table to print
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int x, first;
+
+	x = 0;
+	first = 1;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (x = 0; x < ht->size; x++)
+	{
+		node = ht->array[a];
+		if (node != NULL)
+		{
+			while (node != NULL)
+			{
+				if (!first)
+				{
+					printf(", ");
+				}
+				printf("'%s': '%s'", node->key, node->value);
+				first = 0;
+				node = node->next;
+			}
+		}
+	}
+	printf("}\n");
+}
+
