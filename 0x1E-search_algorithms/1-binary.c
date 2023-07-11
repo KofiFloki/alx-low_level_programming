5c38fff770ec8abbe99336a4358d5b62ad293578
KofiFloki <Jeremiah.martey@yahoo.com>
2023-07-11 15:14:59 -0700

    algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,63 +1,39 @@
 #include "search_algos.h"
 
 /**
- * print_bin - a function that prints the array being searched
- * everytime it changes
- * @array: the array being searched
- * @low: the first index of the array
- * @high: the last index of the array
- */
-
-void print_bin(int *array, size_t low, size_t high)
-{
-	unsigned int a = 0;
-
-	printf("Searching in array: ");
-	for (a = low; a <= high; a++)
-	{
-		if (a != high)
-			printf("%d ", array[a]);
-		else
-			printf("%d\n", array[a]);
-	}
-}
-
-/**
- * binary_search - searches for a value in a sorted array of integers
- * @array: a pointer to the first element of the array
- * @size: the number of elements in the array
- * @value: the value to search for in the array
- * Return: the index which the value is found
- */
-
+  * binary_search - Searches for a value in a sorted array
+  *                 of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
 int binary_search(int *array, size_t size, int value)
 {
-	unsigned int mid, low, high;
-
-	high = size - 1;
+	size_t i, left, right;
 
 	if (array == NULL)
-	{
 		return (-1);
-	}
 
-	for (low = 0; low <= high;)
+	for (left = 0, right = size - 1; right >= left;)
 	{
-		print_bin(array, low, high);
-		mid = (low + high) / 2;
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
 
-		if (array[mid] == value)
-		{
-			return (mid);
-		}
-		else if (array[mid] > value)
-		{
-			high = mid - 1;
-		}
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
 		else
-		{
-			low = mid + 1;
-		}
+			left = i + 1;
 	}
+
 	return (-1);
 }
